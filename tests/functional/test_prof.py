cf0ef6dca32655163bb77dbed17aecdc65d028cb
Dipin Garg <dgargdipin@gmail.com>
2021-05-11 04:02:14 +0530

    added test for addAssignment

--- tests/functional/test_prof.py
+++ tests/functional/test_prof.py
@@ -31,5 +31,20 @@ def test_addCourseMaterial(test_client,addCourse):
 
     
     
+def test_Assignment(test_client,addCourse):
+    from pprint import pprint
+    assert addCourse[0].status_code==200
+    title=''.join(random.choice(string.ascii_lowercase) for i in range(10))
+    response=test_client.post(addCourse[1],data=dict(title=title,\
+    details="Course on data structures, assignment 1",deadline="2021-05-15T03:57",\
+    submit2="Submit"),follow_redirects=True)
+    soup = BeautifulSoup(response.data, 'lxml')
+    assert response.status_code==200
+    courseNoteCards=soup.find_all('div',class_='assignmentCard')
+    titles=[a.h5.text.strip().split()[0] for a in courseNoteCards]
+    print(titles)
+    assert title in titles
 
+    
+    
 
