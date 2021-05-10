2a0502e42d3e6d55fec2f917807df00a58f92ef3
Dipin Garg <dgargdipin@gmail.com>
2021-05-11 03:41:10 +0530

    added tests for addCourse, addCourseNote

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
+from werkzeug import test
 import pytest
 from cms import app, db
 from cms.models import User,Professor
 
+import cms.branch_helper as branch_helper
+
 
 @pytest.fixture(scope='module')
 def new_user():
-    user = User('Dipin','dgargdipin@gmail.com','abc','1',1)
+    user = Professor("Professor 2","prof2@gmail.com","prof2",1)
     return user
 
 
@@ -24,6 +27,10 @@ def test_client():
 def init_database(test_client):
     # Create the database and the database table
     # Insert user data
+    db.create_all()
+    db.session.add_all(branch_helper.create_branch_array())
+    db.session.commit()
+
     user1 = User('Dipin','dgargdipin@gmail.com','abc','1',1)
     user2 = Professor("Professor 1","prof1@gmail.com","prof1",2)
     db.session.add(user1)
@@ -32,17 +39,40 @@ def init_database(test_client):
     # Commit the changes for the users
     db.session.commit()
 
-    yield db # this is where the testing happens!
+    yield# this is where the testing happens!
 
     db.drop_all()
 
 
-@pytest.fixture(scope='function')
-def login_default_user(test_client):
+@pytest.fixture(scope='module')
+def login_default_user(test_client,init_database):
     test_client.post('/login',
-                     data=dict(email='patkennedy79@gmail.com', password='FlaskIsAwesome'),
+                     data=dict(email='prof1@gmail.com', password='prof1'),
                      follow_redirects=True)
 
     yield  # this is where the testing happens!
 
     test_client.get('/logout', follow_redirects=True)
+
+
+from bs4 import BeautifulSoup
+import bs4
+
+@pytest.fixture(scope='module')
+def addCourse(test_client,init_database,login_default_user):
+    response=test_client.post('/',data=dict(name="Data Structures",\
+    course_code="C123",available_for=[1,2],details="Course on data structures",\
+    submit="Add Course"),follow_redirects=True)
+    assert response.status_code==200
+    soup=BeautifulSoup(response.data,'lxml')
+    courses=soup.find_all('a',class_='courseLinks')
+    course_names=[a.text for a in courses]
+    print(course_names)
+    url=""
+    assert "Data Structures" in course_names
+    for course in courses:
+        if course.text=="Data Structures":
+            url=course['href']
+            response=test_client.get(url)
+            break
+    yield response,url
